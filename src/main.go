5ce68ad487056cfbe901d6d9e813b602b1be9f86
SebastianSanchez22 <sebastiansaor22@gmail.com>
2022-11-23 01:10:28 -0500

    Operadores básicos en Go

--- src/main.go
+++ src/main.go
@@ -4,37 +4,38 @@ import "fmt"
 
 func main() {
 
-	// Declaración de constantes
-	const pi float64 = 3.14
-	const pi2 = 3.1415
-
-	fmt.Println("pi:", pi)
-	fmt.Println("pi2:", pi2)
-
-	// Declaración de variables enteras
-	base := 12
-	var altura int = 14
-	var area int
-
-	fmt.Println(base, altura, area)
-
-	// Zero values
-	var a int
-	var b float64
-	var c string
-	var d bool
-
-	fmt.Println(a, b, c, d)
-
 	// Area cuadrado
 	const baseCuadrado = 10
 	areaCuadrado := baseCuadrado * baseCuadrado
 	fmt.Println("Area cuadrado :", areaCuadrado)
 
-	// Declarar varias variables al tiempo
-	x1, x2, x3 := 1, 2, 3
-	fmt.Println(x1)
-	fmt.Println(x2)
-	fmt.Println(x3)
+	x := 10
+	y := 50
+
+	// Suma
+	result := x + y
+	fmt.Println("Suma:", result)
+
+	// Resta
+	result = y - x
+	fmt.Println("Resta:", result)
+
+	// Multiplicación
+	result = x * y
+	fmt.Println("Multiplicación:", result)
+
+	// División
+	result = y / x
+	fmt.Println("División:", result)
+
+	// Módulo
+	result = y % x
+	fmt.Println("Módulo:", result)
+
+	// Incremental y decremental
+	x++
+	fmt.Println("Incremental:", x)
+	x--
+	fmt.Println("Decremental:", x)
 
 }

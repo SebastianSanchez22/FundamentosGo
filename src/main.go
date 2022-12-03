f2b91f94c25409ad17e0839b98aaa14e74b76bfc
SebastianSanchez22 <sebastiansaor22@gmail.com>
2022-12-03 16:40:17 -0500

    Ciclo for

--- src/main.go
+++ src/main.go
@@ -2,33 +2,27 @@ package main
 
 import "fmt"
 
-func normalFunction(message string) {
-	fmt.Println(message)
-}
-
-func tripleArgument(a, b int, c string) {
-	fmt.Println(a, b, c)
-}
-
-func returnValue(a int) int {
-	return a * 2
-}
-
-func doubleReturn(a int) (c, d int) {
-	return a, a * 2
-}
-
 func main() {
-	normalFunction("Hola mundo")
-	tripleArgument(1, 2, "hola")
 
-	value := returnValue(2)
-	fmt.Println(value)
+	// For condicional
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
 
-	value1, value2 := doubleReturn(2)
-	fmt.Println("Value1 y value2:", value1, value2)
+	fmt.Printf("\n")
 
-	// Si solo me interesa retornar uno de los dos valores:
-	value3, _ := doubleReturn(3)
-	fmt.Println("Value3:", value3)
+	// For while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	// For forever
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
+
 }
